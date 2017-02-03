00cc4ef77144ba483609554262de75e548a9e961
pesparza <pesparza@PESPARZA-LAP.oradev.oraclecorp.com>
2017-02-03 11:49:42 -0600

    Removing Dog option in the menu

--- src/Zoo/Zoo.java
+++ src/Zoo/Zoo.java
@@ -52,7 +52,6 @@ public class Zoo {
             menu += "1) Create a Polar Bear\n";
             menu += "2) Create a Lion\n";
             menu += "3) Create a Rabbit\n";
-            menu += "4) Create a Dog\n";
             menu += "8) Print my Zoo\n";
             menu += "9) Exit\n";
 
